bed2e57b4abe2c16bfda9ff460f7a927f2261de3
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-07-20 10:32:59 +0300

    star rating js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,17 @@
-function feedBack() {
-    const name = document.getElementById(#name).value;
-    const email = document.getElementById(#email).value;
-    document.getElementById(#button).innerHTML =( "name" + "your feedback has been received")
-}
+const allStars = document.querySelectorAll('.star');
+allStars.forEach( (star,i)=> {
+    star.onclick = function() {
+        let current_star_level = i+1;
+        allStars.forEach( (star,j) =>{
+            if (current_star_level >= j+1) {
+                star.innerHTML = '&#9733'
+            } else {
+                star.innerHTML = '&#9734';
+            }
+            
+        })
+    }
+});
+
 
 
