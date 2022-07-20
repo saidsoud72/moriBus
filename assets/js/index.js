dacf81acf0d04cc06fa20d75f43d87e7c0310aec
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-07-20 09:42:58 +0300

    js part of my star rating

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function feedBack() {
+    const name = document.getElementById(#name).value;
+    const email = document.getElementById(#email).value;
+    document.getElementById(#button).innerHTML =( "name" + "your feedback has been received")
+}
+
+
