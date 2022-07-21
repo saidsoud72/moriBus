e9deb6bd496c0c797b8075612c79b1519bd3e660
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-07-21 08:53:50 +0300

    add templates

--- moriApp/urls.py
+++ moriApp/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('ticketing/', views.ticketing, name='ticketing'),
+    # path('ticketing/', views.ticketing, name='ticketing'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('booking/', views.booking, name='booking'),
 ]
--- moriApp/views.py
+++ moriApp/views.py
@@ -6,5 +6,16 @@ def home(request):
     return render(request, 'index.html')
 
 
-def ticketing(request):
-    return render(request, 'ticketing.html')
+# def ticketing(request):
+#     return render(request, 'ticketing.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def booking(request):
+    return render(request, 'bookings.html')
